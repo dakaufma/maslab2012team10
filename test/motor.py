a4e394b49b89534f41b2f9d2951202d02f2261de
David Kaufman <chessdog@gmail.com>
2013-01-08 15:07:53 -0500

    Missed this file in the last commit. This test file is currently identical to checkpoint1.

--- test/motor.py
+++ test/motor.py
@@ -7,24 +7,24 @@ import arduino
 sleepTime = 2
 
 ard = arduino.Arduino()
-right = arduino.Motor(ard, 11, 53, 41) #(arduino, currentPin, directionPin, pwmPin)
-left = arduino.Motor(ard, 10, 37, 35)
+right = arduino.Motor(ard, 14, 42, 2) #(arduino, currentPin, directionPin, pwmPin)
+left = arduino.Motor(ard, 15, 43, 3)
 ard.run()  # Start the Arduino communication thread
 
 while True:
 	print "forward"
 	right.setSpeed(127)
-#	left.setSpeed(127)
+	left.setSpeed(127)
 	time.sleep(sleepTime)
 	print "stop"
 	right.setSpeed(0)
-#	left.setSpeed(0)
+	left.setSpeed(0)
 	time.sleep(sleepTime)
 	print "back"
 	right.setSpeed(-127)
-#	left.setSpeed(-127)
+	left.setSpeed(-127)
 	time.sleep(sleepTime)
 	print "stop"
 	right.setSpeed(0)
-#	left.setSpeed(0)
+	left.setSpeed(0)
 	time.sleep(sleepTime)
