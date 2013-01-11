6548feee3a65326476a93be26140a59b55bcfe59
David Kaufman <chessdog@gmail.com>
2013-01-11 16:52:04 -0500

    Vision works on red balls and is fast enough for the checkoff (about 10 times per second). After detecting contours it classifies sufficiently large contours as balls and computes their bounding circles. Bounding circles are ideal for vision because they look the same from all angles.
    TODO:
    -reformat to put everything in methods
    -video feed from camera
    -green balls
    -more complicated stuff (green button vs ball, locate tower, etc)

--- test/cvTest.py
+++ test/cvTest.py
@@ -2,9 +2,10 @@
 
 #intended to be run as `python ctTest.py ../reference_images/*`
 
-import cv2.cv as cv
+import cv2
 import sys
 import math
+import numpy
 
 redHue = 0
 redSat = 255
@@ -18,80 +19,87 @@ def distFromColor(distImg, hue, sat, val, colorHue, colorSat, colorVal, hueWeigh
 	hueWeight *= scale
 	satWeight *= scale
 	valWeight *= scale
-	for row in range(0,distImg.rows):
-		for col in range(0,distImg.cols):
-			h = cv.Get2D(hue, row, col)[0]
-			s = cv.Get2D(sat, row, col)[0]
-			v = cv.Get2D(val, row, col)[0]
-			cv.Set2D(distImg, row, col, (math.sqrt(
-				(min(abs(h-colorHue),180-abs(h-colorHue))) ** 2 * hueWeight +
-				(s-colorSat)                               ** 2 * satWeight +
-				(v-colorVal)                               ** 2 * valWeight)
-			))
+	hi = hue.item
+	si = sat.item
+	vi = val.item
+	for row in xrange(0,distImg.shape[0]):
+		for col in xrange(0,distImg.shape[1]):
+			h = hi(row, col) - colorHue
+			s = si(row, col) - colorSat
+			v = vi(row, col) - colorVal
+			if h < -90:
+				h+=180
+			elif h > 90:
+				h-=180
+			distImg.itemset((row, col), math.sqrt(h*h*hueWeight + s*s*satWeight + v*v*valWeight))
+			
 
 def mouseListener(event, x, y, flags, img):
-	if event==cv.CV_EVENT_LBUTTONDOWN:
-		print "Click at (" + str(x) + ", " + str(y) + "):\tValue " + str(cv.Get2D(img, y, x))
+	if event==cv2.EVENT_LBUTTONDOWN:
+		print "Click at (" + str(x) + ", " + str(y) + "):\tValue " + str(img[y, x])
 
 def displayImage(windowName, img):
-	cv.NamedWindow(windowName)
-	cv.ShowImage(windowName, img)
-	cv.SetMouseCallback(windowName, mouseListener, img)
+	cv2.namedWindow(windowName)
+	cv2.imshow(windowName, img)
+	cv2.setMouseCallback(windowName, mouseListener, img)
 
 if __name__ == '__main__':
 	for fileName in sys.argv[1:]:
 		#load image and shrink to a reasonable size
-		img = cv.LoadImageM(fileName, cv.CV_LOAD_IMAGE_COLOR)
-		smallImg = cv.CreateMat(img.rows/10, img.cols/10, cv.CV_8UC3)
-		cv.Resize(img, smallImg)
-		size = cv.GetSize(smallImg)
+		img = cv2.imread(fileName)
+		smallImg = cv2.resize(img, None, None, .1, .1)
 
 		#Display HSV channels
-		hsv = cv.CreateMat(smallImg.rows, smallImg.cols, cv.CV_8UC3)
-		hue = cv.CreateMat(smallImg.rows, smallImg.cols, cv.CV_8UC1)
-		sat = cv.CreateMat(smallImg.rows, smallImg.cols, cv.CV_8UC1)
-		val = cv.CreateMat(smallImg.rows, smallImg.cols, cv.CV_8UC1)
-		cv.CvtColor(smallImg, hsv, cv.CV_BGR2HSV)
-		cv.Split(hsv, hue, sat, val, None)
+		hsv = numpy.zeros(smallImg.shape, numpy.uint8)
+		hue = numpy.zeros(smallImg.shape[:2], numpy.uint8)
+		sat = numpy.zeros(smallImg.shape[:2], numpy.uint8)
+		val = numpy.zeros(smallImg.shape[:2], numpy.uint8)
+		cv2.cvtColor(smallImg, cv2.COLOR_BGR2HSV, hsv)
+		cv2.split(hsv, (hue, sat, val))
 
-		#require that the hue is red
-		redMask1 = cv.CreateMat(smallImg.rows, smallImg.cols, cv.CV_8UC1)
-		redMask2 = cv.CreateMat(smallImg.rows, smallImg.cols, cv.CV_8UC1)
-		redMask = cv.CreateMat(smallImg.rows, smallImg.cols, cv.CV_8UC1)
-		cv.Threshold(hue, redMask1, redHueMax, 255, cv.CV_THRESH_BINARY_INV)
-		cv.Threshold(hue, redMask2, redHueMin, 255, cv.CV_THRESH_BINARY)
-		cv.Or(redMask1, redMask2, redMask)
+		#require that the hue is red; note that this mask has 0 for red pixels and 255 for non-red
+		redMask1 = numpy.zeros(hue.shape, numpy.uint8) 
+		redMask2 = numpy.zeros(hue.shape, numpy.uint8) 
+		redMask  = numpy.zeros(hue.shape, numpy.uint8) 
+		cv2.threshold(hue, redHueMin, 255, cv2.THRESH_BINARY_INV, redMask1)
+		cv2.threshold(hue, redHueMax, 255, cv2.THRESH_BINARY, redMask2)
+		if redHueMin > redHueMax: #hue wraps around; in this case we want the AND of the two masks
+			cv2.min(redMask1, redMask2, redMask)
+		else: #min < max --> we want the OR of the two masks
+			cv2.max(redMask1, redMask2, redMask)
 
 		#Threshold for sufficiently small red distance
 		#imperically determined 90 is a good threshold
-		redDist = cv.CreateMat(smallImg.rows, smallImg.cols, cv.CV_8UC1)
+		redDist = numpy.zeros(hue.shape, numpy.uint8)
 		distFromColor(redDist, hue, sat, val, redHue, redSat, redVal, 1, 1, 1)
 
-		redMaskedDist = cv.CreateMat(redDist.rows, redDist.cols, cv.CV_8UC1)
-		cv.Set(redMaskedDist, 255)
-		cv.Copy(redDist, redMaskedDist, redMask)
+		redMaskedDist = numpy.zeros(hue.shape, numpy.uint8)
+		cv2.max(redDist, redMask, redMaskedDist)
 
 		#Combine masks for final red ball mask
-		redImg = cv.CreateMat(redDist.rows, redDist.cols, cv.CV_8UC1)
-		cv.Threshold(redMaskedDist, redImg, 90, 255, cv.CV_THRESH_BINARY_INV)
+		redImg = numpy.zeros(redMask.shape, numpy.uint8)
+		cv2.threshold(redMaskedDist, 90, 255, cv2.THRESH_BINARY_INV, redImg)
 		displayImage("red", redImg)
 
 		#Find blobs in red image
-		storage = cv.CreateMemStorage()
-		contour = cv.FindContours(redImg, storage)
+		contours, hierarchy = cv2.findContours(redImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 		nextLargest = None
 		nextArea = 0
-		while contour:
-			area = cv.ContourArea(contour)
-			if area > 100: #minimum size imperically determined
+		index = 0
+		for contour in contours:
+			area = cv2.contourArea(contour)
+			if area > 100: #minimum area imperically determined
 				print area
-				cv.DrawContours(smallImg, contour, cv.RGB(0,0,255), cv.RGB(0,0,255), 20)
+				(x,y), radius = cv2.minEnclosingCircle(contour)
+				cv2.circle(smallImg,(int(x),int(y)), int(radius), (255,0,0), 3)
+				#cv2.drawContours(smallImg, contours, index, (255,0,0), 3)
 			elif area>nextArea:
 				nextArea = area
-				nextLargest = contour
-			contour = contour.h_next()
+				nextLargest = index
+			index += 1
 		if nextArea>0:
 			print "Next largest: "+str(nextArea)
+			cv2.drawContours(smallImg, contours, nextLargest, (0,255,0), 3)
 		print "-------------------------------"
 
 
@@ -103,8 +111,8 @@ if __name__ == '__main__':
 		displayImage("red mask", redMask)
 		displayImage("red dist", redDist)
 		displayImage("red masked dist", redMaskedDist)
-		key = cv.WaitKey()
+		key = cv2.waitKey()
 		if key == 113:
 			break
-	cv.DestroyAllWindows()
+	cv2.destroyAllWindows()
 
